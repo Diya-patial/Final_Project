1db48789c3a628d0bc924283174e537273cdff4c
diya patial <diya49972@gmail.com>
2026-04-24 17:30:02 +0530

    Fix protected routes import casing for Netlify build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Index from "./Index";
-import ProtectedRoute from "./pages/protectedRoutes";
+import ProtectedRoute from "./pages/ProtectedRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const App = () => {
